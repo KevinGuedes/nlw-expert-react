043fc4f1e099a09059a79aff92424c361e6df370
Kevin Guedes <kevinguedes1@gmail.com>
2024-02-15 20:54:17 -0300

    fix: app base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/notetaking/',
+  base: '/nlw-expert-react/',
   plugins: [
     react(),
     VitePWA({
